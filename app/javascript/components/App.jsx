f0258edd6d92bec4a509c6b13a50f53038a74c92
saiprasaddevare <saiprasaddevare12@gmail.com>
2019-07-18 19:30:09 +0530

    Feature :: CMS : Initial setup done

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
   render () {
     return (
       <div>
-        Greeting: {this.props.name}
+        Greeting : {this.props.greeting}
       </div>
     );
   }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,8 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './../components/App.jsx'
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-console.log('Hello World from Webpacker')
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+  ReactDOM.render(<App greeting="Huray ...! Initial Setup done"/>, document.getElementById('app'));
+});
